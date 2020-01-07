2bfd9eb512d41b8c46a018a2859451eaa9d78d21
Karen V <karenvo@usc.edu>
2020-01-07 09:50:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,10 @@
 function saturdayFun(activity="roller-skate") {
   return `This Saturday, I want to ${activity}!`
 }
-saturdayFun()
 
 function mondayWork(activity="go to the office") {
   return `This Monday, I will ${activity}.`
 }
-mondayWork()
 
 function wrapAdjective() {
 
