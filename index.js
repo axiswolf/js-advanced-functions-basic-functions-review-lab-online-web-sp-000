efc6b96b00529be405d48f64a1fc49b35726368c
Karen V <karenvo@usc.edu>
2020-01-07 09:45:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Your code here
-function saturdayFun(activity = "roller-skate") {
+function saturdayFun(activity="roller-skate") {
   console.log(`This Saturday, I want to ${activity}!`)
 }
 
-function mondayWork(activity = "go to the office") {
+function mondayWork(activity="go to the office") {
   console.log(`This Monday, I will ${activity}.`)
 }
 
